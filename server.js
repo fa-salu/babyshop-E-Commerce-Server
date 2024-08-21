388dbdbb5b09cee7a646199d4cf7363f6ccf851f
fasalu <fasalurahman7515@gmail.com>
2024-08-21 20:36:20 +0530

    add config file for connectDB

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require('./config/db')
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes")
 const errorHandler = require("./middleware/errorHandler");
 require("dotenv").config();
 
 const app = express();
+connectDB()
 
 app.use(express.json());
 app.use(errorHandler);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
 
 app.use("/users", userRoutes);
 app.use("/admin", adminRoutes)
